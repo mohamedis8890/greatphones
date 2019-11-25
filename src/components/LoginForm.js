059bfb946fba5f1a3e24b4886abf2df9a807ad16
Mo <mohamed.is8890@gmail.com>
2019-11-25 02:24:40 +0200

    converted to be functional and used Formik

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,129 +1,80 @@
-import React, { Component } from "react";
+import React from "react";
 import { Redirect } from "react-router-dom";
+import * as Yup from "yup";
+import { withFormik, Form, Field } from "formik";
 import { login, getCurrentUser } from "../services/authService";
 import "./LoginForm.css";
 
-const Joi = require("joi-browser");
+function LoginForm({
+	values,
+	errors,
+	touched,
+	isSubmitting,
+	onUserStateChanged
+}) {
+	if (getCurrentUser()) return <Redirect to="/" />;
 
-export default class LoginForm extends Component {
-	state = {
-		data: {
-			email: "",
-			password: ""
-		},
-		errors: {}
-	};
-
-	schema = {
-		email: Joi.string()
-			.email()
-			.required()
-			.label("Email"),
-		password: Joi.string()
-			.required()
-			.label("Password")
-	};
-
-	validate = ({ name, value }) => {
-		const obj = { [name]: value };
-		const schema = { [name]: this.schema[name] };
-		const { error } = Joi.validate(obj, schema);
-		return error ? error.details[0].message : null;
-	};
-
-	validateAll = () => {
-		const options = { abortEarly: false };
-		const { error } = Joi.validate(this.state.data, this.schema, options);
-		if (!error) return null;
-
-		const errors = {};
-		for (let item of error.details) errors[item.path[0]] = item.message;
-		return errors;
-	};
-
-	handleChange = ({ currentTarget: input }) => {
-		const errors = { ...this.state.errors };
-		const errorMessage = this.validate(input);
-		if (errorMessage) errors[input.name] = errorMessage;
-		else delete errors[input.name];
-
-		const data = { ...this.state.data };
-		data[input.name] = input.type === "checkbox" ? input.checked : input.value;
-		this.setState({ data, errors });
-	};
-
-	doSubmit = async () => {
-		try {
-			const user = { ...this.state.data };
-			await login(user.email, user.password);
-
-			this.props.onUserStateChanged();
-			this.props.history.push("/");
-		} catch (ex) {
-			if (ex.response && ex.response.status === 401) {
-				const errors = { ...this.state.errors };
-				errors.email = ex.response.data.message;
-				this.setState({ errors });
-			}
-		}
-	};
-
-	handleSubmit = e => {
-		e.preventDefault();
-
-		const errors = this.validateAll();
-		this.setState({ errors: errors || {} });
-		if (errors) return;
-
-		this.doSubmit();
-	};
-
-	render() {
-		if (getCurrentUser()) return <Redirect to="/" />;
-		const { data, errors } = this.state;
-		return (
-			<div className="login-body ">
-				<form className="form-signin" onSubmit={this.handleSubmit}>
-					<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-					<label htmlFor="inputEmail" className="sr-only">
+	return (
+		<div className="login-body ">
+			<Form className="form-signin">
+				<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+				<div>
+					{errors.email && touched.email && (
+						<div className="alert alert-danger">{errors.email}</div>
+					)}
+					<label htmlFor="email" className="sr-only">
 						Email address
 					</label>
-					<input
+					<Field
 						type="email"
-						id="inputEmail"
-						className="form-control"
-						placeholder="Email address"
 						name="email"
-						value={data["email"]}
-						onChange={this.handleChange}
-						errors={errors["email"]}
-						required
-						autoFocus
+						placeholder="email"
+						className="form-control"
 					/>
-					<label htmlFor="inputPassword" className="sr-only">
+				</div>
+				<div>
+					<label htmlFor="password" className="sr-only">
 						Password
 					</label>
-					<input
-						type="password"
-						id="inputPassword"
-						className="form-control"
-						placeholder="Password"
-						name="password"
-						onChange={this.handleChange}
-						value={data["password"]}
-						errors={errors["password"]}
-						required
-					/>
-					{errors["email"] && (
-						<div className="alert alert-danger">{errors["email"]}</div>
-					)}
-					<div className="checkbox mb-3" />
-					<button className="btn btn-lg btn-primary btn-block" type="submit">
-						Sign in
-					</button>
-					<p className="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
-				</form>
-			</div>
-		);
-	}
+					<Field type="password" name="password" className="form-control" />
+				</div>
+				<div className="checkbox mb-3" />
+				<button
+					className="btn btn-lg btn-primary btn-block"
+					type="submit"
+					disabled={isSubmitting}
+				>
+					Sign in
+				</button>
+				<p className="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+			</Form>
+		</div>
+	);
 }
+
+const FormikLoginForm = withFormik({
+	mapPropsToValues({ email }) {
+		return { email: email || "" };
+	},
+	validationSchema: Yup.object().shape({
+		email: Yup.string()
+			.email("Invalid Email")
+			.required("Email required"),
+		password: Yup.string().required()
+	}),
+	async handleSubmit(values, { props, resetForm, setErrors, setSubmitting }) {
+		try {
+			setSubmitting(true);
+			await login(values.email, values.password);
+			props.onUserStateChanged();
+			setSubmitting(false);
+		} catch (ex) {
+			console.log(ex.response);
+			if (ex.response && ex.response.status === 401) {
+				setErrors(ex.response.data.message);
+			}
+		}
+	}
+})(LoginForm);
+
+export default FormikLoginForm;
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,197 +1,127 @@
-import React, { Component } from "react";
-import Joi from "joi-browser";
+import React from "react";
+import * as Yup from "yup";
+import { withFormik, Form, Field } from "formik";
 
 import registerUser from "../services/userService";
 
-export default class RegisterForm extends Component {
-	state = {
-		data: {
-			name: "",
-			email: "",
-			password: "",
-			passwordConfirm: ""
-		},
-		errors: {}
-	};
-
-	schema = {
-		name: Joi.string()
-			.required()
-			.label("Name"),
-		email: Joi.string()
-			.email()
-			.required()
-			.label("Email"),
-		password: Joi.string()
-			.min(6)
-			.max(30)
-			.required()
-			.label("Password"),
-		passwordConfirm: Joi.string().required()
-	};
-
-	validate = ({ name, value }) => {
-		if (name === "passwordConfirm") {
-			const error = {};
-			const { password } = this.state.data;
-			if (value === "")
-				return (error.passwordConfirm =
-					"Password Confirmation connot be empty");
-			return value !== password
-				? (error.passwordConfirm = "Passwords must match")
-				: null;
-		} else {
-			const obj = { [name]: value };
-			const schema = { [name]: this.schema[name] };
-			const { error } = Joi.validate(obj, schema);
-			return error ? error.details[0].message : null;
-		}
-	};
-
-	validateAll = () => {
-		const options = { abortEarly: false };
-		const { error } = Joi.validate(this.state.data, this.schema, options);
-		if (!error) return null;
-
-		const errors = {};
-		for (let item of error.details) errors[item.path[0]] = item.message;
-		return errors;
-	};
-
-	handleChange = ({ currentTarget: input }) => {
-		const errors = { ...this.state.errors };
-		const errorMessage = this.validate(input);
-		if (errorMessage) errors[input.name] = errorMessage;
-		else delete errors[input.name];
-
-		const data = { ...this.state.data };
-		data[input.name] = input.type === "checkbox" ? input.checked : input.value;
-		this.setState({ data, errors });
-	};
-
-	mapToModel = user => {
-		return {
-			name: user.name,
-			email: user.email,
-			password: user.password,
-			isAdmin: false
-		};
-	};
-
-	doSubmit = async () => {
-		try {
-			const { data: user } = { ...this.state };
-			await registerUser(this.mapToModel(user));
-			this.props.history.push("/");
-		} catch (ex) {
-			console.error(ex);
-		}
-	};
-
-	handleSubmit = e => {
-		e.preventDefault();
-
-		const errors = this.validateAll();
-		this.setState({ errors: errors || {} });
-		if (errors) return;
-
-		this.doSubmit();
-	};
-
-	render() {
-		const { data, errors } = this.state;
-
-		return (
-			<div className="container" style={{ marginTop: "2em" }}>
-				<div className="row">
-					<div className="col-md-12">
-						<form onSubmit={this.handleSubmit}>
-							<div className="form-group">
-								<label htmlFor="inputname">Name</label>
-								<input
-									type="input"
-									id="inputname"
-									className={
-										errors["name"] ? "form-control is-invalid" : "form-control"
-									}
-									name="name"
-									value={data["name"]}
-									onChange={this.handleChange}
-									errors={errors["name"]}
-									required
-									autoFocus
-								/>
+function RegisterForm({ values, errors, touched, isSubmitting }) {
+	return (
+		<div className="container" style={{ marginTop: "2em" }}>
+			<div className="row">
+				<div className="col-md-12">
+					<Form>
+						<div className="form-group">
+							<label htmlFor="name">Name</label>
+							<Field
+								type="input"
+								className={
+									errors["name"] ? "form-control is-invalid" : "form-control"
+								}
+								name="name"
+							/>
+							{touched.name && errors.name && (
 								<div className="invalid-feedback">{errors["name"]}</div>
-							</div>
+							)}
+						</div>
 
-							<div className="form-group">
-								<label htmlFor="email">Email address</label>
-								<input
-									type="email"
-									id="email"
-									className={
-										errors["email"] ? "form-control is-invalid" : "form-control"
-									}
-									name="email"
-									value={data["email"]}
-									onChange={this.handleChange}
-									errors={errors["email"]}
-									required
-									autoFocus
-								/>
+						<div className="form-group">
+							<label htmlFor="email">Email address</label>
+							<Field
+								type="email"
+								className={
+									errors["email"] ? "form-control is-invalid" : "form-control"
+								}
+								name="email"
+							/>
+							{touched.email && errors.email && (
 								<div className="invalid-feedback">{errors["email"]}</div>
-							</div>
-							<div className="form-group">
-								<label htmlFor="password">Password</label>
-								<input
-									type="password"
-									id="password"
-									className={
-										errors["password"]
-											? "form-control is-invalid"
-											: "form-control"
-									}
-									name="password"
-									onChange={this.handleChange}
-									value={data["password"]}
-									errors={errors["password"]}
-									required
-								/>
+							)}
+						</div>
+						<div className="form-group">
+							<label htmlFor="password">Password</label>
+							<Field
+								type="password"
+								className={
+									errors["password"]
+										? "form-control is-invalid"
+										: "form-control"
+								}
+								name="password"
+							/>
+							{touched.password && errors.password && (
 								<div className="invalid-feedback">{errors["password"]}</div>
-							</div>
+							)}
+						</div>
 
-							<div className="form-group">
-								<label htmlFor="passwordConfirm">Confirm Password</label>
-								<input
-									type="password"
-									id="passwordConfirm"
-									className={
-										errors["passwordConfirm"]
-											? "form-control is-invalid"
-											: "form-control"
-									}
-									name="passwordConfirm"
-									onChange={this.handleChange}
-									value={data["passwordConfirm"]}
-									errors={errors["passwordConfirm"]}
-									required
-								/>
+						<div className="form-group">
+							<label htmlFor="passwordConfirm">Confirm Password</label>
+							<input
+								type="password"
+								className={
+									errors["passwordConfirm"]
+										? "form-control is-invalid"
+										: "form-control"
+								}
+								name="passwordConfirm"
+							/>
+							{touched.passwordConfirm && errors.passwordConfirm && (
 								<div className="invalid-feedback">
 									{errors["passwordConfirm"]}
 								</div>
-							</div>
-							<div className="form-group">
-								<button
-									className="btn btn-lg btn-primary btn-block"
-									type="submit"
-								>
-									Register
-								</button>
-							</div>
-						</form>
-					</div>
+							)}
+						</div>
+						<div className="form-group">
+							<button
+								className="btn btn-lg btn-primary btn-block"
+								type="submit"
+								disabled={isSubmitting}
+							>
+								Register
+							</button>
+						</div>
+					</Form>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
+
+const FormikRegisterForm = withFormik({
+	mapPropsToValues(props) {
+		return { email: "", name: "", password: "", passwordConfirm: "" };
+	},
+	validationSchema: Yup.object().shape({
+		email: Yup.string()
+			.email("Invalid Email")
+			.required("Email required"),
+		name: Yup.string().required("Your name is required"),
+		password: Yup.string()
+			.min(6, "Password need to be 6 characters long at least")
+			.max(30, "Password need to be 30 characters at most")
+			.required("Password is Required"),
+		passwordConfirm: Yup.string().when("password", {
+			is: val => (val && val.length > 0 ? true : false),
+			then: Yup.string().oneOf(
+				[Yup.ref("password")],
+				"Both passwords need to be the same"
+			)
+		})
+	}),
+	async handleSubmit(values, { props, resetForm, setErrors, setSubmitting }) {
+		try {
+			setSubmitting(true);
+			const user = {
+				name: values.name,
+				email: values.email,
+				password: values.password,
+				isAdmin: false
+			};
+			await registerUser(user);
+			setSubmitting(false);
+		} catch (ex) {
+			console.log(ex);
+		}
+	}
+})(RegisterForm);
+
+export default FormikRegisterForm;
